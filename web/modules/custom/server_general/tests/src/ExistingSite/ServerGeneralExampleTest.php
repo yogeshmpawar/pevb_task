34434fe28b390d8fa58488aab30b6315dc785818
Yogesh Pawar <yogesh.pawar01@gmail.com>
2023-11-02 18:33:02 +0530

    Added completed tasks and tests.

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralExampleTest.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralExampleTest.php
@@ -46,4 +46,35 @@ class ServerGeneralExampleTest extends ServerGeneralTestBase {
     $this->drupalGet($node->toUrl('edit-form'));
   }
 
+  /**
+   * An example test method; note that Drupal API's and Mink are available.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  public function testGroup() {
+    // Creates a user. Will be automatically cleaned up at the end of the test.
+    $author = $this->createUser();
+
+    // Create a "NewGroup" article. Will be automatically cleaned up at end of
+    // test.
+    $node = $this->createNode([
+      'title' => 'NewGroup',
+      'type' => 'group',
+      'uid' => $author->id(),
+    ]);
+    $this->assertEquals($author->id(), $node->getOwnerId());
+
+    // We can browse pages.
+    $this->drupalGet($node->toUrl());
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->statusMessageContains('if you would like to subscribe to this group called', 'status');
+    $this->clickLink('Click Here');
+
+    // We can login and browse admin pages.
+    $this->drupalLogin($author);
+    $this->drupalGet($node->toUrl('edit-form'));
+  }
+
 }
